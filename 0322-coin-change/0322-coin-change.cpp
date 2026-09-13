6d96f33ceb166d8fd1287c44d2fcc1bdfd3195bc
Adi485455 <aditya7774994577@gmail.com>
2026-09-13 16:43:07 +0530

    Time: 47 ms (35.18%), Space: 49 MB (38.48%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,5 +1,7 @@
 class Solution {
 public:
+
+    /*
     int helper(vector<int>&coins,int idx,int amount,vector<vector<int>>&dp){
 
         //Base case as the question is to find the min so the we return the 1e9 so means the ans is not possible
@@ -25,7 +27,11 @@ public:
         }
         return dp[idx][amount]=min(not_take ,take);
     }
+    */
+
+
     int coinChange(vector<int>& coins, int amount) {
+        /*
         // So the first one is the memoisation approach / reccurance approach
         int n = coins.size();
         vector<vector<int>>dp(n,vector<int>(amount+1,-1));
@@ -35,6 +41,40 @@ public:
             return -1;
         }else{
             return ans;
+        }
+        */
+        // Now the Third Approach of the tabulation 
+
+        // First the base case of the tabulation (similar to the base case of the memoisation just for the loops)
+
+        // As in the recursion we have the base case as returning the division of whatever coins have at the target (remaining) / coins[0]
+        // So in this case here loops we can have the any target value here for the given idx =0 so 
+
+        // Its the base case for the idx ==0 we havn't explicitly described here but in the loop we can see that
+        int n= coins.size();
+        vector<vector<int>>dp(n,vector<int>(amount+1,-1));
+        for (int j=0;j<=amount;j++){
+            if(j % coins[0]==0){
+                dp[0][j] = j / coins[0];
+            }else{
+                dp[0][j] = 1e9;
+            }
+        }
+
+        for(int i=1;i<n;i++){
+            for(int j=0;j<=amount;j++){
+                int not_take = 0 + dp[i-1][j];
+                int take = 1e9;
+                if(coins[i]<= j){
+                    take = 1+dp[i][j-coins[i]];
+                }
+                dp[i][j]= min(take ,not_take);
+            }
+        }
+        if(dp[n-1][amount]>=1e9){
+            return -1;
+        }else{
+            return dp[n-1][amount];
         }
     }
 };
